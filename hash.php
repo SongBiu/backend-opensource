a42328f64f5a40afd8704da717edbec8218453e5
SongBiu <song_pku@foxmail.com>
2018-04-04 21:29:20 +0800

    hash
    修补了一个巨傻逼无比的bug

--- hash.php
+++ hash.php
@@ -1,4 +1,11 @@
 <?php
-	$s = "abcdef";
+	$s = $_REQUEST['str'];
+	if ($s == '') {
+		$ch = '0123456789qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM';
+		$s = '';
+		for ($i = 6; $i > 0; $i--) {
+			$s.=$ch[mt_rand(0, strlen($ch)-1)];
+		}
+	}
 	print(hash("md5", $s));
 ?>
